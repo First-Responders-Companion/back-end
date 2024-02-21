dbc13e3d3026a9c3c24092cfc3b4cf96b4018442
Sankalp Gunturi <sgunturi@cmu.edu>
2024-02-21 15:59:49 -0500

    Add Mongoose Modal

--- src/controllers/exampleControllers.ts
+++ src/controllers/exampleControllers.ts
@@ -1,11 +1,23 @@
 import { RequestHandler } from "express";
+import Example from "../models/Example";
+import createHttpError from "http-errors";
 
 export const getExample: RequestHandler = (req, res, next) => {
   res.json({ message: "Hello World" });
 };
 
-export const getExampleData: RequestHandler = (req, res, next) => {
+export const getExampleData: RequestHandler = async (req, res, next) => {
   const { name, id }: IExampleData = req.body;
 
-  res.json({ name, id });
+  try {
+    const example = await Example.findOne({ name });
+    if (example) return next(createHttpError(406, "Name already exists"));
+
+    const newExample = new Example({ name, id });
+    await newExample.save();
+
+    res.json({ name, id });
+  } catch (error) {
+    return next(createHttpError.InternalServerError);
+  }
 };
