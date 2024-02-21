5f7196754445676610c320e03f9bc37569e7fb2b
Sankalp Gunturi <sgunturi@cmu.edu>
2024-02-20 19:18:47 -0500

    Add Express TypeScript Setup & Error Handling Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log('Hello World');
+import express, { ErrorRequestHandler } from "express";
+import createHttpError from "http-errors";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.use(() => {
+  throw createHttpError(404, "Not Found");
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.log(err.message, err.statusCode);
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Unknown Error" });
+};
+
+app.use(errorHandler);
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
